e7b6c376dfa5c8e9ea7cecda468445e9dfcf2873
Duvan Alejandro Rodriguez Rojas <alejandro64311@gmail.com>
2021-09-05 19:44:14 -0500

    arreglo de vista

--- src/app/simulacion/simulacion.component.ts
+++ src/app/simulacion/simulacion.component.ts
@@ -5,7 +5,8 @@ import {
   INDEX_VALUE_1,
   INDEX_VALUE_2,
   INDEX_DIRECTION,
-  INDEX_NEXT_STATE
+  INDEX_NEXT_STATE,
+  MAX_ITERATIONS
 } from '../../variables';
 @Component({
   selector: 'app-simulacion',
@@ -21,16 +22,16 @@ export class SimulacionComponent implements OnInit {
   private chartListCinta: string[] = [];
   private indexCinta: number = 0;
   private actualState:string;
-
+  private finalText =""; 
+ 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
-      cinta: new FormControl('11010*10100'),
-      quintuplas: new FormControl('q1 0 1 r q2\nq2 0 0 r q3'),
+      cinta: new FormControl(''),
+      quintuplas: new FormControl('0 _ 1 r 1\n1 _ 1 r 2\n2 _ 1 r 3\n3 _ _ l 2\n2 1 _ l 1\n1 1 _ l 0\n0 1 _ r 0'),
       result: new FormControl(''),
-    });
-    
+    }); 
   }
 
   execute() {
@@ -40,10 +41,10 @@ export class SimulacionComponent implements OnInit {
   }
 
   executeIterations() {
-    var finished = 10;
+    var finished = 0;
     try {
       this.executInitialSteep();
-      while (finished<10) {
+      while (finished<MAX_ITERATIONS) {
         this.executSteep();
         // this.goToNextState();
   
@@ -59,7 +60,12 @@ export class SimulacionComponent implements OnInit {
   }
 
   initVariables() {
+    this.arrayQuintuplas = [];
+    this.chartListCinta  = [];
+    this.finalText =  "";
     this.cinta = this.form.controls["cinta"].value;
+ 
+ 
     this.setInitialIndex();
     this.buildCintaVariables();
     var rows = this.converLinesToStringList();
@@ -96,7 +102,10 @@ export class SimulacionComponent implements OnInit {
   }
 
   buildCintaVariables() {
-    this.cinta = this.cinta.replace("*", "");
+    this.cinta = this.cinta.replace(/\s/g, "_");
+    this.cinta = this.cinta.replace(/\*/g, "");
+    this.cinta = this.cinta==""?"_":this.cinta;
+
     for (let i = 0; i < this.cinta.length; i++)
       this.chartListCinta.push(this.cinta[i]);
 
@@ -114,30 +123,61 @@ export class SimulacionComponent implements OnInit {
 
     let newChar = tupla[INDEX_VALUE_2];
     this.chartListCinta[this.indexCinta] = newChar;
-    this.indexCinta = tupla[INDEX_DIRECTION]=="r"?this.indexCinta++:this.indexCinta--;
+    this.indexCinta = tupla[INDEX_DIRECTION]=="r"?this.indexCinta+1:this.indexCinta-1;
  
     this.actualState = tupla[INDEX_NEXT_STATE];
 
-    console.log("this.chartListCinta",this.chartListCinta);
+    this.finalText += this.getIterationText();
 
+    this.form.controls["result"].setValue(this.finalText);
+      
+  }
+
+  getIterationText(){
+    var text = "\n"+this.chartListCinta.toString();
+    var text = text.replace(/_/g,"").replace(/,/g,"");
+    return  text;
   }
   executSteep() {
-    let char = this.chartListCinta[this.indexCinta];
+    let char = this.chartListCinta[this.indexCinta]==undefined ? "_" : this.chartListCinta[this.indexCinta];
     let indexTupla = this.arrayQuintuplas.findIndex(x => x[INDEX_VALUE_1] == char && x[INDEX_STATE]==this.actualState);
     let tupla = this.arrayQuintuplas[indexTupla];
 
     let newChar = tupla[INDEX_VALUE_2];
+
+    // this.addChartToCinta(newChar,tupla);
     this.chartListCinta[this.indexCinta] = newChar;
-    this.indexCinta = tupla[INDEX_DIRECTION]=="r"?this.indexCinta++:this.indexCinta--;
+    this.indexCinta = tupla[INDEX_DIRECTION]=="r"?this.indexCinta+1:this.indexCinta-1;
  
     this.actualState = tupla[INDEX_NEXT_STATE];
 
-    console.log("this.chartListCinta",this.chartListCinta);
-    
+     
+    this.finalText += this.getIterationText();
+    this.form.controls["result"].setValue(this.finalText);
   }
   goToNextState() {
   
     
+  }
+  addChartToCinta(newChar,tupla)
+  {
+    var chart = this.getChar(newChar,tupla);
+    if(chart!="")
+      this.chartListCinta[this.indexCinta] = chart;
+
+    
+  }
+  getChar(newChar, tupla){
+    if(newChar=="_"){
+      if(tupla[INDEX_DIRECTION]=="r")
+        this.indexCinta--;
+
+        else
+        this.indexCinta++;
+
+        return "";
+    }
+    return newChar;
   }
 
 
--- src/variables.ts
+++ src/variables.ts
@@ -3,3 +3,6 @@ export const INDEX_VALUE_1 = 1;
 export const INDEX_VALUE_2 = 2;
 export const INDEX_DIRECTION = 3;
 export const INDEX_NEXT_STATE = 4;
+
+
+export const MAX_ITERATIONS = 100;
